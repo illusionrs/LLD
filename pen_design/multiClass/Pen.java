391dc3b9efac464808f03be2deed2b597af246dd
“illusions” <rajeevshukla255@gmail.com>
2023-01-08 15:04:29 +0530

    package name modified

---
+++ pen_design/multiClass/Pen.java
@@ -0,0 +1,12 @@
+package pen_design.multiClass;
+
+import pen_design.singleClass.PenType;
+
+public abstract class Pen {
+
+    private String name;
+    private String brand;
+    private PenType penType;
+    private Double price;
+    
+}
